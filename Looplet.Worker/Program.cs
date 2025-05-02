f72992ace61a0f0f90bed3284df7fe3667904bf4
Isaac De La Lama <isaacdelalama1013@gmail.com>
2025-05-01 23:05:38 -0500

    adding swagger

--- Looplet.Worker/Program.cs
+++ Looplet.Worker/Program.cs
@@ -19,6 +19,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddSingleton<IJobFactory, JobFactory>();
 builder.Services.AddSingleton<IConfiguration>(configuration);
 
