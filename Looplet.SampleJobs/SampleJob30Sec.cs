eb7498facd2df9d2fa263573c91b5d5b351e21d4
Isaac De La Lama <isaacdll99@gmail.com>
2025-05-05 23:22:32 -0500

    added 30 second sample job

---
+++ Looplet.SampleJobs/SampleJob30Sec.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using Looplet.Abstractions.Interfaces;
+
+namespace Looplet.SampleJobs;
+
+public class SampleJob30Sec : IJob
+{
+    public async Task ExecuteAsync(JsonElement? parameters, CancellationToken cancellationToken)
+    {
+        // Simulates a 30-second job
+        Console.WriteLine("SampleJob30Sec started.");
+        await Task.Delay(30000, cancellationToken);
+        Console.WriteLine("SampleJob30Sec finished.");
+    }
+}
